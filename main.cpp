50a93eb210a95e08467ce098896723eb247b1563
Karo <93450785+slvrkngt@users.noreply.github.com>
2026-04-06 12:45:55 +0200

    Stworzenie okna

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <raylib.h>
+
+int main() {
+    InitWindow(800, 800, "Minesweeper"); // Stworzenie okna gry (width, height, title)
+    SetTargetFPS(60); // frames per second
+
+    // Game Loop
+    while (WindowShouldClose()==false) {
+
+
+        BeginDrawing();
+        EndDrawing();
+    }
+
+    CloseWindow(); // Zamknięcie okna gry
+    return 0;
+}
